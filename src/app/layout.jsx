6e691b3163ca064666dcf7c6b261d52736d92027
Ryvann <ryanisnjoroge@gmail.com>
2026-08-31 15:21:32 +0300

    feat: integrate Google Tag Manager for enhanced tracking

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,20 +12,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RXVG6HTCSV"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RXVG6HTCSV');
-          `}
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M5VWT9PL');`}
         </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className="bg-bg text-text font-sans antialiased m-0 overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M5VWT9PL"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div id="root" className="w-full mx-auto min-h-screen">
           <Navbar />
           {children}
