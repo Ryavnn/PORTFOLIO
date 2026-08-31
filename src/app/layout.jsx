f020700d4edd0372fe06145e85fb81dc112f11f6
Ryvann <ryanisnjoroge@gmail.com>
2026-08-31 15:12:12 +0300

    feat: add Google Analytics scripts to the layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import '../index.css';
 import Navbar from '../components/Layout/Navbar';
 import Footer from '../components/Layout/Footer';
@@ -10,6 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RXVG6HTCSV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RXVG6HTCSV');
+          `}
+        </Script>
+      </head>
       <body className="bg-bg text-text font-sans antialiased m-0 overflow-x-hidden">
         <div id="root" className="w-full mx-auto min-h-screen">
           <Navbar />
